9fccdb5f8292ae3eba6ac7c998b3d09331c27bfd
Ziv <zkarmi25@gmail.com>
2020-01-03 11:08:21 -0500

    initial commit

---
+++ client/index.js
@@ -0,0 +1,11 @@
+// import css
+import '../public/index.css';
+
+// import react
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>This is my store</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js', // assumes your entry point is the index.js in the root of your project folder
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public/'), // assumes your bundle.js will also be in the root of your project folder
+    filename: 'bundle.js'
+  },
+  devServer: {
+    publicPath: '/',
+    contentBase: './public', // we determined this is the only part we need in here
+    hot: true
+  },
+  devtool: 'source-maps',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      }
+    ]
+  }
+}
